0936e017b66389f4368e3d1b4bc82ec4fda886c4
duola8789 <duola8789@126.com>
2017-04-16 21:35:49 +0800

    1 完成导航跟随移动 2 完成滚动监听 3 优化页面布局 4 完成企业介绍部分页面

--- readme.md
+++ readme.md
@@ -7,3 +7,17 @@ The website for PE-IDEA industry design company which is established by my ex-co
 2. 字体
 3. 轮播图尺寸1067*650，为保证效果须统一尺寸
 4. 滚动监听
+
+
+
+需要总结的知识：
+1. 在线字体
+2. font-awesome
+3. animate.min.css
+4. 出现的效果
+5. jQuery hover() 方法
+6. bootstrap滚动监听和affix
+7. 导航固定
+8.当只定义position：absolute时，top,bottom，left,right都不指定，则left，top值与原
+文档流位置一致。即跟当它static时的位置一样，但是不占位。
+9. 自己编写的导航条滚动函数
--- source/scripts/pe1.js
+++ source/scripts/pe1.js
@@ -1,8 +1,43 @@
 /**
  * Created by JESSIEJOE on 2017/4/4.
  */
+var $myCarousel1 = $('#myCarousel1'),
+    $carousel1Height = $myCarousel1.height(),
+    $myHeader = $("#myHeader"),
+    $collapse = $('#example-navbar-collapse');
 
 //首页轮播图循环时间
-$('#myCarousel1').carousel({
-    interval: 2500
+$(function(){
+    $myCarousel1.carousel({
+        interval: 2500
+    });
+
+// 增加图标抖动动画类
+    $(".service-title").hover(function(){
+        $(this).find('i').addClass('swing')
+    },function(){
+        $(this).find('i').removeClass('swing')
+    });
+
+// 导航滚动
+    $(document).scroll(function(){
+        var $carousel1Height = $myCarousel1.height();
+        if($(this).scrollTop() > $carousel1Height){
+            $myHeader.css({
+                'position' : 'fixed',
+                'top' : 0
+            })
+        }
+        else{
+            $myHeader.css({
+                'position' : 'absolute',
+                'top' : $carousel1Height
+            })
+        }
+    });
+    $(window).resize(function(){
+        $(document).triggerHandler('scroll')
+    })
 });
+
+
